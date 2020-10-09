f263dcf1c2733c148637d3950b38c7ced52b0eaf
Daniel Waiguru <danielirungu91@gmail.com>
2020-10-09 13:00:57 +0300

    init Timber logger

---
+++ app/src/main/java/tech/danielwaiguru/placediary/App.kt
@@ -0,0 +1,14 @@
+package tech.danielwaiguru.placediary
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initTimber()
+    }
+    private fun initTimber(){
+        Timber.plant(Timber.DebugTree())
+    }
+}
