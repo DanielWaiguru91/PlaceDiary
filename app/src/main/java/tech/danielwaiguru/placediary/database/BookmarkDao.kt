d4b34e6043b493d681422f8209a304f7ea5f70d2
Daniel Waiguru <danielirungu91@gmail.com>
2020-10-12 10:55:38 +0300

    add BookmarkDao interface to access the database

---
+++ app/src/main/java/tech/danielwaiguru/placediary/database/BookmarkDao.kt
@@ -0,0 +1,21 @@
+package tech.danielwaiguru.placediary.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import tech.danielwaiguru.placediary.models.Bookmark
+
+@Dao
+interface BookmarkDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addBookmark(bookmark: Bookmark)
+    @Query("SELECT * FROM places")
+    suspend fun getAllBookmarks(): LiveData<List<Bookmark>>
+    @Query("SELECT * FROM places WHERE id = :bookmarkId")
+    suspend fun getBookmark(bookmarkId: Int): Bookmark
+    @Query("SELECT * FROM places WHERE id = :bookmarkId")
+    suspend fun getBookmarkLiveDate(bookmarkId: Int): LiveData<Bookmark>
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateBookmark(bookmarkId: Int)
+    @Delete
+    suspend fun deleteBookmark(bookmark: Bookmark)
+}
--- app/src/main/java/tech/danielwaiguru/placediary/models/Bookmark.kt
+++ app/src/main/java/tech/danielwaiguru/placediary/models/Bookmark.kt
@@ -1,11 +1,11 @@
-package tech.danielwaiguru.placediary.database
+package tech.danielwaiguru.placediary.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "places")
 data class Bookmark (
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val placeId: String? = null,
     val address: String,
